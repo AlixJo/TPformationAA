0a0732f739677d4e1c5ead911b2a676cdda0f746
Unknown <alix-jourdan@hotmail.fr>
2018-08-10 12:16:17 +0200

    mapping alix 2

--- formation-model/src/main/java/sopra/promo404/formation/model/Formation.java
+++ formation-model/src/main/java/sopra/promo404/formation/model/Formation.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -14,6 +15,7 @@ import javax.persistence.Transient;
 
 public class Formation {
 	@EmbeddedId
+	@OneToOne
 	private FormationId id;
 	@Column (name="duration")
 	private int duree;
--- formation-model/src/main/java/sopra/promo404/formation/model/Matiere.java
+++ formation-model/src/main/java/sopra/promo404/formation/model/Matiere.java
@@ -7,6 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 @Entity
@@ -22,7 +23,7 @@ public class Matiere {
 	private int duree;
 	@Transient
 	private Difficulte difficulte;
-	@Transient
+	@ManyToMany
 	private List<Formateur> formateurs = new ArrayList<>();
 
 	public Matiere() {
